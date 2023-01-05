7faceab623d8abb7220b77f40a13c37f4ae90753
TheDro <andrewsc32@protonmail.com>
2023-01-05 18:02:07 -0500

    Fixed drumeo stuff

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-
 function every(delay, callback) {
 
   let complete = callback()
@@ -12,22 +11,29 @@ function every(delay, callback) {
 }
 
 every(2000, () => {
+  let toggleSettings = document.getElementById('toggle-settings')
   let barHighlight = document.getElementById('highlight-bar-option')
   let noteHighlight = document.getElementById('highlight-notes')
   let shape = document.getElementById('playhead-shape')
+
 
   if (barHighlight === null) {
     return false
   }
 
   setTimeout(() => {
-    barHighlight.checked = true
-    barHighlight.dispatchEvent(new Event('change'))
-    noteHighlight.checked = false
-    noteHighlight.dispatchEvent(new Event('change'))
-    shape.value = '4'
-    shape.dispatchEvent(new Event('change'))
-  }, 2000)
+    toggleSettings.dispatchEvent(new Event('pointerdown'))
+    setTimeout(() => {
+      barHighlight.checked = true
+      barHighlight.dispatchEvent(new Event('change'))
+      noteHighlight.checked = false
+      noteHighlight.dispatchEvent(new Event('change'))
+      shape.value = '4'
+      shape.dispatchEvent(new Event('change'))
+      toggleSettings.dispatchEvent(new Event('pointerdown'))
+    }, 500)
+    
+  }, 1000)
   
 
   return true
